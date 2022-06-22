0e8c77e0f4d04f006fbf6d0f1ade4256bfca11be
Nicholas Manha <nickrmanha@gmail.com>
2022-06-21 18:11:01 -0700

    added about

--- Desktop/rob portfolio/main.js
+++ Desktop/rob portfolio/main.js
@@ -1,13 +1,44 @@
+const inputs = document.querySelectorAll(".input");
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "nickrmanha@gmail.com",
+        Password : "Martiangaming11",
+        To : 'nickrmanha@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
 $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
-            $('.logo').addClass('active');
-            $('.logo2').addClass('active');
+
         } else {
-            $('.logo2').removeClass('active');
+
             $('.navbar').removeClass('active');
-            $('.logo').removeClass('active');
+
         }
     });
 });
